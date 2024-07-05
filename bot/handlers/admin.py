50952c9da0738ab87930c8fabd24692e0a6ad65c
Ayrelia <nikitakoteqka12@gmail.com>
2024-07-05 13:49:39 +0300

    pull

---
+++ bot/handlers/admin.py
@@ -0,0 +1,28 @@
+from config import Bot, F, Router, FSInputFile, types, FSMContext, State, bot, CallbackData, FSInputFile
+from markups.adminmarkup import *
+import filters.filtersbot as filtersbot
+from sql_function import databasework
+import filters.filtersbot as filtersbot
+    
+router = Router()
+
+router.message.filter(filtersbot.AdminCheck()) # привязываем фильтр к роутеру
+router.callback_query.filter(filtersbot.AdminCheck()) # привязываем фильтр к роутеру
+
+
+
+
+
+
+
+@router.message(F.text == '/admin', F.chat.type == 'private')
+async def start(message: types.Message, state: FSMContext):
+    #photo = FSInputFile('kwork8/photo/start_message.jpg')
+    await state.clear()
+    markup = admin_markup()
+    await message.answer(f'💎 Вы попали в админ-панель', reply_markup=markup)
+    
+    
+    
+    
+admin = router
